0eb0601268839570c6ba42f87c7bebd465130e30
sadatnazarli <sedat.nazarli@gmail.com>
2024-08-12 12:58:44 +0000

    header file added

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Standard Libraries */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+/* Function Prototypes */
+void execute_command(char *command);
+
+#endif /* MAIN_H */
+
