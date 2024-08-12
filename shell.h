b91952d2a8e2773ff6b3e1681aedc563fcef36f3
sadatnazarli <sedat.nazarli@gmail.com>
2024-08-12 16:38:00 +0000

    task

---
+++ shell.h
@@ -0,0 +1,18 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+#define MAX_ARGS 1024
+void print_prompt(void);
+void parse_line(char *line, char **args);
+int handle_builtins(char **args);
+void execute_command(char **args);
+char *get_full_path(char *command);
+#endif /* SHELL_H */
