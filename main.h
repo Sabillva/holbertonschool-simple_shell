deff3226dbbf241a11a02a961d1963d18b31cd5d
sadatnazarli <sedat.nazarli@gmail.com>
2024-08-12 13:08:13 +0000

    new files

--- main.h
+++ main.h
@@ -1,14 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Standard Libraries */
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 
 /* Function Prototypes */
-void execute_command(char *command);
+char *read_line(void);
+char **split_line(char *line);
+int execute(char **args);
 
 #endif /* MAIN_H */
 
