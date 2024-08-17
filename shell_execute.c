b24ad4f2c99834785c07e8f085076a239cf58e60
sadatnazarli <sedat.nazarli@gmail.com>
2024-08-17 09:41:09 +0000

    path added

--- shell_execute.c
+++ shell_execute.c
@@ -1,127 +1,123 @@
-#include "myshell.h"
-
-/* Declare the global variable for full path */
-char *cmd_fullpath;
+#include "main.h"
 
 /**
- * display_error - displays error for command not found
- * @cmd_input: command inserted
+ * split - splits a string into an array of strings
+ * @command: string to split
+ * Return: returns an array of strings
  */
-
-void display_error(char *const cmd_input[])
+char **split(char *command)
 {
-    fprintf(stderr, "./myshell: 1: %s: not found\n", cmd_input[0]);
-    free(*cmd_input);
-    exit(127);
+	char *token, **tokens;
+	int i = 0;
+
+	tokens = malloc(sizeof(char *) * SIZE);
+	if (!tokens)
+	{
+		perror("Malloc Error: ");
+		exit(98);
+	}
+	token = strtok(command, " \t\n");
+	while (token)
+	{
+		tokens[i++] = strdup(token);
+		token = strtok(NULL, " \t\n");
+	}
+	tokens[i] = NULL;
+	return (tokens);
 }
 
 /**
- * find_env_path - get environment variable by name
- * @name: name of environment variable
- * @envp: pointer to environment variables
- * Return: pointer to environment variable
+ * pre_execute - prepares and executes a command
+ * @command: command to execute
+ * @tmp: temporary variable for memory management
+ * @status: status
+ * Return: void
  */
-
-char *find_env_path(const char *name, char **envp)
+void pre_execute(char *command, char *tmp, int *status)
 {
-    char **env, *separator;
+	struct stat st;
+	char **arr, *path;
 
-    for (env = envp; *env != NULL; env++)
-    {
-        separator = strchr(*env, '=');
-        if (separator != NULL)
-        {
-            if (strncmp(*env, name, separator - *env) == 0)
-            {
-                return (separator + 1);
-            }
-        }
-    }
-    return (NULL);
+	arr = split(command);
+	if (command[0] == '/' || command[0] == '.')
+	{
+		if (stat(arr[0], &st) != 0)
+		{
+			free_arr(arr);
+			free(tmp);
+			return;
+		}
+		path = strdup(command);
+	}
+	else
+		path = get_path(arr, command);
+	execute(arr, path, status, tmp);
 }
 
 /**
- * resolve_path - resolves the correct path for a given command
- * @cmd: given input to check for path
- * @cmd_input: argument array
- * @envp: pointer to environment variables
- * Return: pointer to the resolved command path or NULL if failed
- */
-
-char **resolve_path(char *cmd, char **cmd_input, char **envp)
+ * execute - executes a command
+ * @arr: array of commands
+ * @path: path of the command
+ * @status: status
+ * @tmp: temporary variable for memory management
+ * Return: void
+*/
+void execute(char **arr, char *path, int *status, char *tmp)
 {
-    char *current_path, *temp_path;
-    char *path_tok;
-    size_t cmd_len = strlen(cmd);
+	pid_t pid = fork();
 
-    cmd_fullpath = NULL;
-
-    if (strchr(cmd, '/') != NULL && access(cmd, F_OK) == 0)
-    {
-        cmd_input[0] = cmd;
-        return (cmd_input);
-    }
-
-    path_tok = NULL;
-    current_path = find_env_path("PATH", envp);
-    temp_path = strdup(current_path);
-    path_tok = strtok(temp_path, ":");
-
-    while (path_tok)
-    {
-        cmd_fullpath = malloc(cmd_len + strlen(path_tok) + 2);
-        sprintf(cmd_fullpath, "%s/%s", path_tok, cmd);
-        if (access(cmd_fullpath, F_OK) == 0)
-        {
-            cmd_input[0] = cmd_fullpath;
-            free(temp_path);
-            return (cmd_input);
-        }
-        path_tok = strtok(NULL, ":");
-        free(cmd_fullpath);
-    }
-    free(temp_path);
-    return (NULL);
+	if (pid == -1)
+	{
+		free(tmp);
+		perror("Error");
+	}
+	else if (pid == 0)
+	{
+		if (execve(path, arr, environ) == -1)
+		{
+			perror("Error execve");
+			free_arr(arr);
+			free(tmp);
+			free(path);
+			exit(1);
+		}
+	}
+	else
+	{
+		waitpid(pid, status, 0);
+		if (WIFEXITED(*status))
+			*status = WEXITSTATUS(*status);
+		free(tmp);
+	}
+	free(path);
+	free_arr(arr);
 }
 
 /**
- * run_command - function to execute commands
- * @cmd_input: input from user
- * @envp: environment path
- * Return: -1 if failed and 0 if success
+ * free_arr - frees an array of strings
+ * @arr: array to free
+ * Return: void
  */
-
-int run_command(char *const cmd_input[], char **envp)
+void free_arr(char **arr)
 {
-    pid_t pid;
-    int status, i;
-    char **resolved_cmd = resolve_path(cmd_input[0], (char **) cmd_input, envp);
+	int i = 0;
 
-    if (resolved_cmd != NULL)
-    {
-        pid = fork();
-        if (pid < 0)
-        {
-            perror("fork failed");
-            return (-1);
-        } else if (pid == 0)
-        {
-            if (find_env_path("PATH", envp) == NULL && access(cmd_input[0], F_OK) != 0)
-                display_error(cmd_input);
-            execve(resolved_cmd[0], cmd_input, envp);
-            for (i = 0; cmd_input[i] != NULL; i++)
-                free(cmd_input[i]);
-            exit(EXIT_FAILURE);
-        }
-        wait(&status);
-        free(cmd_fullpath);
-    } else
-        display_error(cmd_input);
-    if (errno == -1)
-    {
-        free(*cmd_input);
-        exit(2);
-    }
-    return (0);
+	while (arr[i])
+		free(arr[i++]);
+	free(arr);
 }
 
+/**
+ * print_env - prints the environment
+ * Return: void
+*/
+void print_env(void)
+{
+	int i = 0;
+
+	while (environ[i])
+	{
+		printf("%s\n", environ[i]);
+		i++;
+	}
+}
--- shell_main.c
+++ shell_main.c
@@ -1,134 +1,66 @@
-#include "myshell.h"
-
-/* Declare global variables */
-char **env_vars;
-char *cmd_input;
-
+#include "main.h"
 /**
- * display_env - prints the environment variables
- * Return: zero
+ * main - main function of the shell
+ * Return: returns 0 on success
  */
-
-int display_env(void)
+int main(void)
 {
-    int i = 0;
+	char *command, *tmp = NULL;
+	int read, status = 0;
+	size_t size;
 
-    if (env_vars == NULL)
-        return (-1);
-    while (env_vars[i])
-    {
-        printf("%s\n", env_vars[i++]);
-    }
-    return (0);
+	while (1)
+	{
+		command = NULL;
+		if (isatty(STDIN_FILENO))
+			printf("$ ");
+		fflush(stdout);
+		read = getline(&command, &size, stdin);
+		if (read == -1)
+		{
+			free(command);
+			break;
+		}
+		command[read - 1] = '\0';
+		tmp = command;
+		if (command == NULL)
+		{
+			free(tmp);
+			break;
+		}
+		while (command[0] == ' ' || command[0] == '\t')
+			command++;
+
+		if (command[0] == '\n' || command[0] == '\0')
+		{
+			free(tmp);
+			continue;
+		}
+		if (exit_and_env(command, &status))
+			continue;
+		pre_execute(command, tmp, &status);
+	}
+	return (status);
 }
-
 /**
- * tokenize_input - tokenizes the input command
- * @cmd_input: command from the user
- * @envp: environment path
+ * exit_and_env - handle exit and env function
+ * @command: command
+ * @status: status
+ * Return: true if command is env false otherwise
  */
-
-void tokenize_input(char cmd_input[], char **envp)
+bool exit_and_env(char *command, int *status)
 {
-    char *args[11];
-    char *token = strtok(cmd_input, " ");
-    int arg_count = 0;
-
-    while (token != NULL && arg_count < 10)
-    {
-        args[arg_count++] = token;
-        token = strtok(NULL, " ");
-    }
-    if (arg_count > 11)
-    {
-        perror("Error: Too many arguments");
-        return;
-    }
-    args[arg_count] = NULL;
-    if (arg_count > 0)
-    {
-        if (envp != NULL)
-            run_command(args, envp);
-        else
-        {
-            fprintf(stderr, "./myshell: 1: %s: not found\n", args[0]);
-            exit(127);
-        }
-    }
+	if (strcmp(command, "env") == 0)
+	{
+		*status = 0;
+		print_env();
+		free(command);
+		return (true);
+	}
+	if (strcmp(command, "exit") == 0)
+	{
+		free(command);
+		exit(*status);
+	}
+	return (false);
 }
-
-/**
- * get_user_input - prompts user for input and processes it
- * @cmd_input: pointer to the command
- * @size: size of the command
- */
-
-void get_user_input(char **cmd_input, size_t *size)
-{
-    size_t read_bytes;
-
-    read_bytes = getline(cmd_input, size, stdin);
-    if ((int) read_bytes == EOF)
-    {
-        if (isatty(STDIN_FILENO) != 0)
-            printf("\n");
-        if (*cmd_input != NULL)
-            free(*cmd_input);
-        exit(EXIT_SUCCESS);
-    }
-    if ((*cmd_input)[read_bytes - 1] == '\n')
-        (*cmd_input)[read_bytes - 1] = '\0';
-}
-
-/**
- * main - Main function for the shell (interactive or non-interactive)
- * @argc: argument count
- * @argv: argument vector
- * @envp: environmental variable
- * Return: 1 if command fails
- */
-
-int main(int argc, char *argv[], char **envp)
-{
-    size_t size;
-
-    env_vars = envp;
-    cmd_input = NULL;
-    (void) argv;
-    if (argc > 1)
-    {
-        printf("./myshell: command does not exist\n");
-        exit(EXIT_FAILURE);
-    }
-    while (1)
-    {
-        size = 0;
-        if (isatty(STDIN_FILENO))
-            printf("myshell($) ");
-        get_user_input(&cmd_input, &size);
-        if (strcmp(cmd_input, "exit") == 0)
-        {
-            free(cmd_input);
-            exit(EXIT_SUCCESS);
-        }
-        if (strcmp(cmd_input, "env") == 0)
-        {
-            display_env();
-            free(cmd_input);
-            exit(EXIT_SUCCESS);
-        }
-        if (find_env_path("PATH", envp) == NULL)
-        {
-            if (cmd_input[0] != '/')
-            {
-                fprintf(stderr, "./myshell: 1: %s: not found\n", cmd_input);
-                free(cmd_input);
-                exit(127);
-            }
-        }
-        tokenize_input(cmd_input, envp);
-        free(cmd_input);
-    }
-    return (0);
-}
-
